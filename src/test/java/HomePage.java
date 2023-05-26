c55937410170ec6026d8b7b1add7b49a8a4c8b2c
abhi.sakthivel <abhi.sakthivel@lalamove.com>
2023-05-27 04:13:26 +0800

    Chnaged Class name

--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class HomePage1 {
+public class HomePage {
     static WebDriver driver;
     static String navigationSection1 = "(//button[@type='button'])[3]";
     static String navigationSection2 = "(//button[@type='button'])[5]";
